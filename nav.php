877f40aa2f0ce1c5324a773c082b72e66339b6ee
pinda-lall <rupinder.lall@outlook.com>
2018-12-10 15:41:24 +0000

    nav.php added

---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="create.php">Create new member</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+  </div>
+</nav>
